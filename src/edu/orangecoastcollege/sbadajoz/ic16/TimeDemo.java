962c69fab06d817548060a1f13b7fba28e9c44a7
Severiano Badajoz <sevebadajoz@gmail.com>
2016-10-26 12:28:21 -0700

    edited TimeDemo and cleaned Time.isAM()

--- src/edu/orangecoastcollege/sbadajoz/ic16/TimeDemo.java
+++ src/edu/orangecoastcollege/sbadajoz/ic16/TimeDemo.java
@@ -17,7 +17,7 @@ public class TimeDemo {
         a.setHour(14);
         System.out.println("The hour of time " + a + "is: " + a.getHour());
         b.setMinute(59);
-        System.out.println("The minute of time" + b + "is: " + b.getMinute());
+        System.out.println("The minute of time " + b + "is: " + b.getMinute());
         c.setTime(14, 0);
         checkEquals(a, c);
         if(a.isAM()) System.out.println(a + " is an AM time");
